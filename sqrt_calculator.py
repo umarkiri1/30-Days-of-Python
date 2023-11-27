750f652c5d57156c6ada60d4014a22f8b4b42bd4
umarkiri1 <152039979+umarkiri1@users.noreply.github.com>
2023-11-27 16:17:40 +0000

    import math
    def calculate_square_root(number):
        return math.sqrt(number)
    
    number = float(input("Enter a number: "))
    result = calculate_square_root(number)
    print(f"The square root of {number} is {result}")

--- sqrt_calculator.py
+++ sqrt_calculator.py
@@ -1,1 +1,10 @@
+import math
+
+def calculate_square_root(number):
+    return math.sqrt(number)
+
+# Example usage:
+number = float(input("Enter a number: "))
+result = calculate_square_root(number)
+print(f"The square root of {number} is {result}")
 
